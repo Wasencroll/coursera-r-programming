291b343dfaab7850bfc0060f51180d5e1b8231ca
Alexander Croll <alexandercroll@icloud.com>
2016-11-04 00:32:32 +0100

    updating evaluation file

--- week1/syntax-crash-course/evaluation.R
+++ week1/syntax-crash-course/evaluation.R
@@ -1,6 +1,6 @@
-x <- 5 ## nothing printed
-x ## auto-printing
-print(x) ## explicit printing
+x <- 5      ## nothing is printed
+x           ## auto printing (implicit)
+print(x)    ## explicit printing
 
-y <- 11:30 ## creating a sequence with :
-y
+y <- 11:30  ## creating a sequence with :
+y           ## auto-printing
